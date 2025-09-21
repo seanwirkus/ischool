2e5439029906ae7ec98a009ed3479b898d719d53
seanwirkus <123360930+seanwirkus@users.noreply.github.com>
2025-09-21 15:06:15 -0700

    Initial Commit

---
+++ Sean/SeanApp.swift
@@ -0,0 +1,32 @@
+//
+//  SeanApp.swift
+//  Sean
+//
+//  Created by Sean Wirkus on 9/21/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SeanApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
